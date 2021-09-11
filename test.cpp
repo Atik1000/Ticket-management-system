874dc037a653201db50a82e2b6f2ca72b11cf805
Atik1000 <foysalkazi45@gmail.com>
2021-09-11 20:26:16 +0600

    add

--- test.cpp
+++ test.cpp
@@ -1,56 +1,69 @@
-// admin panel reg and login 
-// user panel reg and login
-// user panel logout
-// Hi bro how are you ??
+/*  C++ Program for Username and Password Registration System  */
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include<fstream>
 using namespace std;
 
-class User{
-    private:
-    string email;
-    string password;
-    string user_id;
-    ifstream user_file("user.txt");
+struct mail
+{
+    char un[50];             // user name
+    char pd[50];             // passsword
+    void reg(int);
+} obj[5];
 
-    public:
-    string user_name;
+void mail::reg(int k)
+{
+    int i=k;
+    cout<<"\nEnter user name :: ";
+    cin>>un;
+    cout<<"\nEnter password :: ";
+    cin>>pd;
 
-    User(){
-        email = "";
-        password = "";
-        user_id = "";
-        user_name = "";
+    ofstream filout;
+    filout.open("registration.txt",ios::app|ios::binary);
+    if(!filout)
+    {
+        cout<<"\nCannot open file\n";
     }
-    User(string email, string password){
-        this->email = email;
-        this->password = password;
-    }
-    string login(){
-        if(email == " " && password == " "){
-            cout << "Enter email and password" << endl;
-    }
-        else if(this->email != NULL && this->password != NULL){
-            while(!user_file.eof()){
-                getline(user_file, email, ',');
-                getline(user_file, password, '\n');
-                if(email == this->email && password == this->password){
-                    cout << "Login Successful" << endl;
-                    return user_id;
-                }
-            }
-            cout << "Login Failed" << endl;
-            return "";
+    else
+    {
+        cout<<"\n";
+        filout.write((char *)&obj[i],sizeof(mail));
+        filout.close();
     }
 
-};
+    cout<<"\n...........You are now registered.......... \n\n";
 
-int main(){
-    string email, password;
-    cout << "Enter email and password" << endl;
-    cin >> email >> password;
-    User user(email, password);
+}   // end of sign up or register func
 
-    user.login();
-    return 0;
+int main()
+{
+    int t;
+    cout<<"\nEnter Registration Details for User 1 :: \n";
+    obj[0].reg(0);
+    cout<<"\nEnter Registration Details for User 2 :: \n";
+    obj[1].reg(1);
+    cout<<"\nEnter Registration Details for User 3 :: \n";
+    obj[2].reg(2);
+
+    mail obj2;
+
+    ifstream filein;
+    filein.open("registration.txt",ios::in|ios::binary);
+    if(!filein)
+    {
+        cout<<"\nUnable to open file to read\n";
+    }
+    else
+    {
+        cout<<"\nRegistered Details of All Users :: \n";
+        filein.read((char *)&obj2,sizeof(obj2));
+        while(filein)
+        {
+            cout<<"\nUsername :: "<<obj2.un<<"\nPasswword :: "<<obj2.pd<<"\n";
+            filein.read((char *)&obj2,sizeof(obj2));
+        }
+            //filein.close();
+    }
+        return 0;
 }
