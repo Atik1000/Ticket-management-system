362707f81eaba774f7ab0861eee89dd921b227fa
Atik1000 <foysalkazi45@gmail.com>
2021-09-12 21:33:16 +0600

    more feature added

--- app.cpp
+++ app.cpp
@@ -1,66 +1,3 @@
-
-// 1.Admin feature:
-
-//     1.1.User module :Admin login/reg and user management
-
-//     1.2.Category module: Admin can add/delete/modify category
-//     AC/non Ac/chair
-//     1.3.Bus Type module:Local/Direct/Express
-//     1.4.Bus module: Name,Number,Type,Route,Time,Price,Seat,Seat Available,Seat Booked,Seat Reserved,Seat Cancelled,Seat Sold,Seat Cancelled
-
-//     1.5.Bus Route module:
-//         1.5.1.Route Name
-//         1.5.2.Route Number
-//         1.5.3.Route Type
-//         1.5.4.Route Start
-//         1.5.5.Route End
-//         1.5.6.Route Time
-//         1.5.7.Route Price
-//         1.5.8.Route Seat
-
-//     1.6.Bus path module:
-//         1.6.1.Bus id
-//         1.6.2.start route
-//         1.6.3.end route
-//         1.6.4.total station
-//         1.6.5.stations
-//         1.6.6.Total fair
-
-//     1.7.Bus Schedule module:
-//         1.7.1.Bus id
-//         1.7.2.start time
-//         1.7.3.end time
-
-//     1.8.Bus Seat booking module:
-//         1.8.1.Bus id
-//         1.8.2.Seat Number
-//         1.8.3.Seat Price
-//         1.8.4.Seat Status
-//         1.8.5.Seat Booked
-//         1.8.6.Seat Reserved
-//         1.8.7.Seat Cancelled
-//         1.8.8.Seat Sold
-//         1.8.9.Passenger id
-
-// 2.User feature:
-//     2.1.User registration:
-
-//         2.1.1.Reg/login
-
-//     2.1.Bus searching module:
-//         2.1.1.Bus Name
-//         2.1.2.Bus Number
-//         2.1.3.Route Search
-//         2.1.4.Schedule Search
-
-//     2.2.Bus booking module:
-//         2.2.1.Bus Name
-//         2.2.2.Bus Number
-//         2.2.3.Seat Number
-//         2.2.4.Seat Price
-//         2.2.5.Seat Status
-//         2.2.6.Path
-
 #include <bits/stdc++.h>
 using namespace std;
 // class Admin
@@ -192,7 +129,7 @@ public:
         output.open("user.txt", ios_base::app);
         output << uName << " " << pass << endl;
         output.close();
-        cout << "Registration complete!" << endl;
+        cout << "===================================Registration successful======================================" << endl;
     }
 
     string login(string logName, string logPass)
@@ -209,7 +146,7 @@ public:
             istringstream iss(line);
             if (iss >> uName >> pass && uName == logName && pass == logPass)
             {
-                cout << "Login successful" << endl;
+                cout << "==========================================Login successful============================" << endl;
                 input.close();
                 return uName;
             }
